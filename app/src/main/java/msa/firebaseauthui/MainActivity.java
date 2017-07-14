28b95c0dc6b165599f323bf9df4e4dc7743a02e5
Abhi Muktheeswarar <abeemukthees@gmail.com>
2017-07-14 13:15:28 +0530

    add option to force show auth picker

--- app/src/main/java/msa/firebaseauthui/MainActivity.java
+++ app/src/main/java/msa/firebaseauthui/MainActivity.java
@@ -2,12 +2,22 @@ package msa.firebaseauthui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Collections;
+
+import msa.auth.AuthUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseApp firebaseApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +25,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        firebaseApp = FirebaseApp.initializeApp(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                login();
             }
         });
+    }
+
+    private void login() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            showToastMessage("Already Signed in");
+        } else {
+            // No user is signed in
+            startActivityForResult(
+                    AuthUI.getInstance(firebaseApp)
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                            .setAvailableProviders(Collections.singletonList(new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()))
+                            .alwaysShowAuthMethodPicker(true)
+                            .build(), 1);
+        }
+
+    }
+
+    protected void showToastMessage(String message) {
+        if (message != null && message.length() > 0)
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
 }
